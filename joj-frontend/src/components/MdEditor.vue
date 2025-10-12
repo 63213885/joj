220d8c18896d20b096a7c706bb843e41e125301e
jzz <63213885@qq.com>
2025-10-12 13:55:53 +0800

    引入MdEditor组件

---
+++ joj-frontend/src/components/MdEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import { Editor, Viewer, withDefaults, defineProps } from "@bytemd/vue-next";
+import { ref } from "vue";
+
+/**
+ * 定义组件类型
+ */
+interface Props {
+  value: string;
+  handleChange: (value: string) => void;
+}
+
+const plugins = [
+  gfm(),
+  highlight(),
+  // Add more plugins here
+];
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (value: string) => {
+    console.log(value);
+  },
+});
+</script>
+
+<style scoped></style>
