d091ca47d72ad6267fc9ee81d85e0cc69ba4e9ca
jzz <63213885@qq.com>
2025-11-01 15:20:46 +0800

    model模块搬完

---
+++ joj-backend-microservice/joj-backend-model/src/main/java/com/joj/jojbackendmodel/model/dto/question/JudgeCase.java
@@ -0,0 +1,20 @@
+package com.joj.jojbackendmodel.model.dto.question;
+
+import lombok.Data;
+
+/**
+ * 题目样例
+ */
+@Data
+public class JudgeCase {
+
+    /**
+     * 输入样例
+     */
+    private String input;
+
+    /**
+     * 输出样例
+     */
+    private String output;
+}
